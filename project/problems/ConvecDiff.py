7c13f8b50a1cef91d96d101a96a7c954267526a5
Michael Wathen <michael.wathen@stfc.ac.uk>
2019-04-29 12:27:43 +0100

    Adding convection diffusion problem class

--- project/problems/ConvecDiff.py
+++ project/problems/ConvecDiff.py
@@ -1,7 +1,6 @@
 from .BaseClass import BaseClass
 from .Poisson import Poisson
-import firedrake as fd
-from sympy import *
+from firedrake import inner, dx, dot, grad, DirichletBC, FacetNormal, nabla_grad, VectorFunctionSpace
 from project.utils import Expression
 
 
@@ -11,11 +10,28 @@ class ConvecDiff(Poisson):
         super(ConvecDiff, self).__init__(V, nu)
 
     def advection(self):
-        w = Expression(self.wind, self.V)
+        n = FacetNormal(self.mesh)
+        if len(self.wind) > 1:
+            element = self.V._ufl_element
+
+            degree = element.degree(0)
+            space_type = repr(element)[0]
+            element_type = element.family()
+
+            mesh = self.V.mesh()
+            V = VectorFunctionSpace(mesh, element_type, degree)
+        else:
+            V = self.V
         if self.opt['form'] == 'linear':
-            return fd.inner((fd.grad(self.u) * w), self.v) * fd.dx
-        if self.opt['form'] == 'bilinear':
-            return fd.inner((fd.grad(w) * w), self.v) * fd.dx
+            w_ = Expression(self.wind, V)
+            u_ = self.sol
+        elif self.opt['form'] == 'bilinear':
+            w_ = Expression(self.wind, V)
+            u_ = self.u
+        elif self.opt['form'] == 'nonlinear':
+            w_ = self.sol
+            u_ = self.sol
+        return inner(dot(w_, nabla_grad(u_)), self.v) * dx
 
     @property
     def form(self):
@@ -23,59 +39,16 @@ class ConvecDiff(Poisson):
             self._form = self.laplacian() + self.advection()
         return self._form
 
-    def advection_exact_differentiator(self, u, w):
-        element_dim = len(u)
-        w_len = len(w)
-        # while len(w) < 3:
-        #     if w_len == 1:
-        #         w.append(w[0])
-        #     else:
-        #         w.append('0')
-        mesh_dim = self.V.mesh().cell_dimension()
-        if mesh_dim == 1:
-            x = symbols('x')
-        elif mesh_dim == 2:
-            x, y = symbols('x y')
-        elif mesh_dim == 3:
-            x, y, z = symbols('x y z')
-        print([x, y])
-
-        if element_dim >= 1:
-            A1 = eval(w[0]) * diff(eval(u[0]), x) + eval(w[1]) * diff(eval(u[0]), y) + eval(w[1]) * diff(eval(u[0]), z)
-            out = [str(A1)]
-        if element_dim >= 2:
-            A2 = eval(w[0]) * diff(eval(u[1]), x) + eval(w[1]) * diff(eval(u[1]), y) + eval(w[1]) * diff(eval(u[1]), z)
-            out.append(str(A2))
-        if element_dim >= 3:
-            A3 = eval(w[0]) * diff(eval(u[2]), x) + eval(w[1]) * diff(eval(u[2]), y) + eval(w[1]) * diff(eval(u[2]), z)
-            out.append(str(A3))
-        return super().convert_sympy_firedrake_expresion(out)
-
-    def convect(self, w, u, x):
-        return eval(w) * diff(eval(u), x)
-
     @property
     def f(self):
         if not self._f:
             if self.uE:
-                L = self.laplacian_exact_differentiator(self.uE)
-                A = self.advection_exact_differentiator(self.uE, self.wind)
-                self._f = [x[0] + ' + ' + x[1] for x in zip(L, A)]
+                laplacian = self.ExactSol.laplacian(self.uE)
+                advection = self.ExactSol.advection(self.uE, self.wind)
+                laplacian = self.ExactSol.scalar_mult(-self.nu, laplacian)
+                self._f = [x[0] + ' + ' + x[1]
+                           for x in zip(laplacian, advection)]
         return self._f
-
-    @property
-    def rhs(self):
-        if not self._rhs:
-            self._rhs = fd.inner(Expression(self.f, self.V), self.v) * fd.dx
-        return self._rhs
-
-    @property
-    def bcs(self):
-        if not self._bcs:
-            self._bcs = [fd.DirichletBC(self.V,
-                                        Expression(self.uB, self.V),
-                                        'on_boundary')]
-        return self._bcs
 
     @property
     def wind(self):
