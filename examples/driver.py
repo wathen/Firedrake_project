0c0c3f0050ccfe8551f191e5744bc355e8fad459
Michael Wathen <mwathen@gmail.com>
2019-05-02 08:48:05 +0100

    Driver file updates

--- examples/driver.py
+++ examples/driver.py
@@ -10,14 +10,3 @@ for i in range(4):
     V = FiniteElementSpaces.TaylorHood(mesh)
     # V = VectorFunctionSpace(mesh, 'CG', 1)
     p = Stokes(V, nu=1., opt={'form': 'bilinear'})
-    p.uE = ['x', 'y']
-    p.pE = ['x']
-    # p.k = 1.0
-    assemble(p.form)
-    solve(p.form == p.rhs, p.sol, bcs=p.bcs,
-          solver_parameters={'ksp_type': 'preonly',
-                             'pc_type': 'lu'})
-    print(errornorm(misc.Expression(p.uE, V, 1), p.sol))
-    e = Function(V)
-    e.dat.data[:] = p.sol.dat.data - misc.Expression(p.uE, V).dat.data
-WcmQLSGd2vMWAT11@PH.RC
